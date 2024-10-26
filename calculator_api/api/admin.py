8da8ee78924263e599731f0dec773a2d16641ecc
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-10-26 19:15:17 +0300

    Update admin.py

--- calculator_api/api/admin.py
+++ calculator_api/api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import RequestLog, CalculationResult
 
-# Register your models here.
+admin.site.register(RequestLog)
+admin.site.register(CalculationResult)
